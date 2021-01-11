7114703362ee87f81abb70bc867be8125851ccba
igor <estudostec@hotmail.com>
2021-01-11 00:01:19 -0300

    img-prot

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Porjeto da água!!</Text>
+      <Text>ÁGUA É VIDA</Text>
       <StatusBar style="auto" />
     </View>
   );
